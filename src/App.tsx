645f9d1ca2df1a8dab85dba751c76ce90ac9d257
Alicanefee <alicanefe61@gmail.com>
2025-10-18 01:08:13 +0900

    Integrate BookingProvider and BookingRoot components

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,38 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { BookingProvider } from "./components/booking/BookingProvider";
+import BookingCard from "./components/booking/BookingCard";
+import StickyBookingBar from "./components/booking/StickyBookingBar";
+import { useBooking } from "./components/booking/BookingProvider";
 
 const queryClient = new QueryClient();
+
+const BookingRoot = () => {
+  const { bookingPackage } = useBooking();
+  return (
+    <>
+      {bookingPackage && <BookingCard selectedPackage={bookingPackage} slotsRemaining={5} offerExpiry={undefined} />}
+      <StickyBookingBar selectedPackage={bookingPackage ?? 'Essential'} />
+    </>
+  );
+};
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <BookingProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+        <BookingRoot />
+      </BookingProvider>
     </TooltipProvider>
   </QueryClientProvider>
 );
